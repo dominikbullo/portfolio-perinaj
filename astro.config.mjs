d25142b307b29df9747338fae2794d080d7d0876
Dominik Bullo <dominobullo@gmail.com>
2026-05-12 23:40:18 +0200

    feat: seo stuff v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://perinaj.com",
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/cv"),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
